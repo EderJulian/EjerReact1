2d27f82931a7dc25fbc200096ff680ea93cd79b9
Eder <julianeder97@gmail.com>
2024-01-31 16:51:06 +0100

    Done

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Person from './components/Person'
+import PersonClass from './components/PersonClass'
+import Home from "./components/Home/Home";
+import TheHeader from "./components/TheHeader/TheHeader";
+
+function App() {
+  return (
+    <>
+
+{/* Ejercicio 1.2 */}
+
+      <Person name="Dani" surname='Sopa' age='26'></Person>
+      <Person name='Eder' surname='Julián' age='29'></Person>
+      <Person name='Holy' surname='Onorato' age='32'></Person>
+      
+
+{/* Ejercicio 2 */}
+
+      <TheHeader name='Eder Julián Quintela' age='26'/>
+
+      <Home 
+        section='Sobre mi' 
+        data='Hola, me llamo Eder y tengo 26 años. Estoy realizando un ejercicio de React básico para practicarlo.'
+      />
+      
+      <Home 
+        section='¿Qué tecnologías manejo?' 
+        data='Por ahora estoy aprendiendo a utilizar: HTML, CSS, Javascript, mySQL, MongoDB, Docker, 
+              Node.js y React.'
+      />
+    </>
+  )
+}
+
+export default App
